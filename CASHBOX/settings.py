e220835b146d910fb14705481453fcaae5759fcf
Mesut Kihal <MesutKihal@messaoudkihal@protonmail.com>
2024-10-14 09:46:10 +0000

    fix databases

--- CASHBOX/settings.py
+++ CASHBOX/settings.py
@@ -73,7 +73,12 @@ WSGI_APPLICATION = 'CASHBOX.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 if ENVIRONMENT == "production":
     DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
